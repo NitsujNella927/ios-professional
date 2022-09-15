3fe507ef58ab39b865d38ac94d72cdf93f8ade0c
Justin Allen <jallen@mccallfarms.com>
2022-09-15 16:43:44 -0400

    feat: add stylizing to the text field

---
+++ Password/Password/PasswordTextField.swift
@@ -0,0 +1,67 @@
+//
+//  PasswordTextField.swift
+//  Password
+//
+//  Created by Justin Allen on 9/15/22.
+//
+
+import Foundation
+import UIKit
+
+class PasswordTextField: UIView {
+    
+    let lockImageView = UIImageView(image: UIImage(systemName: "lock.fill"))
+    let textField = UITextField()
+    let placeHolderText: String
+    
+    init(placeHolderText: String) {
+        self.placeHolderText = placeHolderText
+        
+        super.init(frame: .zero)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension PasswordTextField {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemOrange
+        
+        lockImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.isSecureTextEntry = false // reset to true when done
+        textField.placeholder = placeHolderText
+//        textField.delegate = self
+        textField.keyboardType = .asciiCapable
+        textField.attributedPlaceholder = NSAttributedString(string: placeHolderText, attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryLabel])
+        
+    }
+    
+    func layout() {
+        addSubview(lockImageView)
+        addSubview(textField)
+        
+        // lock
+        NSLayoutConstraint.activate([
+            lockImageView.centerYAnchor.constraint(equalTo: textField.centerYAnchor),
+            lockImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+        ])
+        
+        // textfield
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: topAnchor),
+            textField.leadingAnchor.constraint(equalToSystemSpacingAfter: lockImageView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
